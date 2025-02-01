332bc0b3fb52c355451db030960d1ef823304bed
RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>
2025-01-31 19:53:22 -0500

    Add files via upload

---
+++ component/footer.js
@@ -0,0 +1,11 @@
+import 'footer.css';
+
+function Footer(){
+    return(
+        <div classname="Footer">
+            Slow and steady wins the race
+        </div>
+    )
+}
+
+export default Footer;
---
+++ component/header.js
@@ -0,0 +1,11 @@
+import 'header.css'
+
+function Header(){
+    return(
+        <div classname="Header">
+            The Tortoise and the Hare
+        </div>
+    )
+}
+
+export default Header;
